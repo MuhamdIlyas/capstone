f8d15ed78843ea02c3a0946f604046c31fba6082
muhamadilyas17 <lyas170100@gmail.com>
2021-12-10 01:08:06 +0700

    Menata Ulang Posisi File

--- capstone_front_end/src/component/main.js
+++ capstone_front_end/src/component/main.js
@@ -2,6 +2,9 @@ function main() {
     const buttonModal = document.getElementById("modalLabel");
 
     const buttonPredict = document.getElementById('predict');
+
+
+    
 }
 
 export default main;
